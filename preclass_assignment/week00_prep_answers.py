d6e3cd9ffe0f37890ce15f2449fce98ba5854185
s250675 <s250675@dtu.dk>
2025-02-09 19:13:59 +0000

    Updated script with exercises: corrections on ex4 and ex5

--- preclass_assignment/week00_prep_answers.py
+++ preclass_assignment/week00_prep_answers.py
@@ -1,9 +1,10 @@
+#Exercise 1
 def greet(input_name):
     print('Hello,', input_name)
 
 greet('world')
 
-
+#Exercise 2
 def goldilocks(bed_size):
     if bed_size < 140:
         print('Too small!')
@@ -14,8 +15,10 @@ def goldilocks(bed_size):
 
 goldilocks(139)
 goldilocks(140)
+goldilocks(150)
 goldilocks(151)
 
+#Exercise 3
 def square_list(number_list):
     results_list = [None]*len(number_list) # initialize output list
 
@@ -25,40 +28,36 @@ def square_list(number_list):
 
 square_list([1,2,3])
 
+#Exercise 4
 def fibonnaci_stop (max_number):
     if max_number < 1:
         return []  # If max_number is less than 1, return an empty list
     else: 
-        iteration=1
+        iteration=1 #Starts iteration count in 1
         x=1
         next_x=x
         fibonnaci_sequence=[x,next_x]
-        while x <= max_number:
+        while x + next_x <= max_number:
+            x = fibonnaci_sequence[iteration]
             next_x= x + fibonnaci_sequence[iteration-1]
             fibonnaci_sequence.append(next_x)
             iteration += 1
-            x = fibonnaci_sequence[iteration]
     
     print(fibonnaci_sequence)
 
 fibonnaci_stop(30)     
 
+#Exercise 5
 def clean_pitch(x, status):
-    cleaned_angles = []  # List to store cleaned pitch angles
 
-    for pitch, status_value in zip(x, status):
-        if (pitch < 0 or pitch > 90) and status_value > 0:
-            cleaned_angles.append(-999)  # Mark bad values
-        else:
-            cleaned_angles.append(pitch)  # Keep good values as is
-
-    return cleaned_angles
+    for i, status_value in enumerate(status):
+        if status_value > 0 and (x[i]<0 or x[i]>90):
+            x[i]=-999  # Substitutes "bad values" for -999
+    print (x)
 
 # Example:
 x = [-1, 2, 6, 95]
 status = [1, 0, 0, 0]
 
-cleaned = clean_pitch(x, status)
-print(cleaned)
-
+clean_pitch(x, status)
 
