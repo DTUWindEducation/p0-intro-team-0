7c739e537664ff0252142e31ed924b5c7659fb91
8lackt0n <58937215+8lackt0n@users.noreply.github.com>
2025-02-16 14:07:15 +0100

    added test for goldilocsk and tested 5/5 passed

--- test_week2.py
+++ test_week2.py
@@ -18,9 +18,12 @@ def test_greet(capsys):
 def test_goldilocks(capsys):
     """Check goldilocks returns expected output"""
     # given
+    inp = 140
     # when
+    fxn.goldilocks(inp)
+    captured = capsys.readouterr()
     # then
-    assert False  # TODO! Update the contents of this function so it correctly tests goldilocks
+    assert captured.out == 'Just right. :)\n'  # TODO! Update the contents of this function so it correctly tests goldilocks
 
 
 def test_square_list():
@@ -37,8 +40,8 @@ def test_square_list():
 def test_fibonacci_stop():
     """Check fibonacci functions works as expected."""
     # given
-    inp = [10, 22]
-    exp_out = [[1,1,2,3,5,8],[1,1,2,3,5,8,13,21]]
+    inp = 10
+    exp_out = [1,1,2,3,5,8]
     # when
     out = fxn.fibonacci_stop(inp)
     # then
