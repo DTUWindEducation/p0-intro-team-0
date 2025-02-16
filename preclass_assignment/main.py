b540059411300b9de7e5eba07889ae146162434b
Frederiknator <frederikbornemann@gmail.com>
2025-02-16 13:53:43 +0100

    Week 2 homework part 2A

---
+++ preclass_assignment/main.py
@@ -0,0 +1,16 @@
+import functions as fnx
+
+fnx.greet('world')
+
+fnx.goldilocks(139)
+fnx.goldilocks(140)
+fnx.goldilocks(151)
+fnx.goldilocks(150)
+
+print(fnx.square_list([1,2,3]))
+
+print(fnx.fibonacci_stop(30))
+
+x = [-1, 2, 6, 95]
+status = [1, 0, 0, 0]
+print(fnx.clean_pitch(x, status))
